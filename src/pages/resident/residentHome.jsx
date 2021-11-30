d99fe03fc6e31a3ae07228cadf110fb09ef246be
FatimaBZ <begumfatimazohra@gmail.com>
2021-11-29 23:06:19 -0600

    added new css dashboard

--- src/pages/resident/residentHome.jsx
+++ src/pages/resident/residentHome.jsx
@@ -1,4 +1,9 @@
 import "./residentHome.css";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 export default function ResidentHome() {
   return (
@@ -12,7 +17,75 @@ export default function ResidentHome() {
           <h1>Hi Resident!</h1>
           <p>To Proceed Please Select From The Options Below</p>
         </div>
-        <div className="row">
+
+        <Row xs={1} md={4} className="g-4">
+          <Col>
+            <Card body outline color="success" className="mx-auto my-2" border="primary">
+
+
+
+              <Card.Body>
+                <Card.Title><p style={{ textAlign: 'center' }}><i className="fas fa-tools fa-2x"></i></p>
+                  <Card.Title><h4 style={{ textAlign: 'center' }}>Request A Service</h4></Card.Title></Card.Title>
+                <Card.Text>
+                  Whether a property requires any maintenance or a complete onboarding, we get it done.
+                </Card.Text>
+                <p style={{ textAlign: 'center' }}><a href="/resident_request_service"> <Button variant="primary">Click here</Button> </a> </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card body outline color="success" className="mx-auto my-2" border="primary">
+
+
+              <Card.Body>
+                <Card.Title><p style={{ textAlign: 'center' }}><i class="fas fa-list fa-2x"></i></p>
+                  <Card.Title><h4 style={{ textAlign: 'center' }}>Service Details</h4></Card.Title></Card.Title>
+                <Card.Text>
+                  Whenever a building requires any maintenance or tenant issues, Lunar Management is ready to help.
+                </Card.Text>
+                <p style={{ textAlign: 'center' }}><a href="/resident_service_details"> <Button variant="primary">Click here</Button> </a> </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card body outline color="success" className="mx-auto my-2" border="primary">
+              <Card.Body>
+                <Card.Title><p style={{ textAlign: 'center' }}><i class="fas fa-file fa-2x"></i></p>
+                  <Card.Title> <h4 style={{ textAlign: 'center' }}>Upload File</h4></Card.Title></Card.Title>
+
+                <Card.Text>
+                  Visitors can submit a form for inquiry stating which apartment they want to visit to.
+                </Card.Text>
+                <p style={{ textAlign: 'center' }}><a href="/file_Upload"> <Button variant="primary">Click here</Button> </a> </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card body outline color="success" className="mx-auto my-2" border="primary">
+              <Card.Body>
+                <Card.Title><p style={{ textAlign: 'center' }}><i class="fas fa-comments fa-2x"></i></p>
+                  <Card.Title> <h4 style={{ textAlign: 'center' }}>Chat With Us</h4></Card.Title></Card.Title>
+
+                <Card.Text>
+                  Visitors can submit a form for inquiry stating which apartment they want to visit to.
+                </Card.Text>
+                <p style={{ textAlign: 'center' }}><a href="/chatwithus"> <Button variant="primary">Click here</Button> </a> </p>
+              </Card.Body>
+            </Card>
+          </Col>
+
+        </Row>
+
+
+
+
+
+
+
+
+
+        {/* <div className="row">
           <div className="service-col">
             <h3>
               <a className="btn btn-link" href="/resident_request_service">
@@ -36,7 +109,7 @@ export default function ResidentHome() {
               </a>
             </h3>
           </div> */}
-          <div class="divider"/>
+          {/* <div class="divider"/>
           
 
           <div className="service-col" id="validate">
@@ -52,7 +125,7 @@ export default function ResidentHome() {
               </a>
             </h3>
           </div>
-        </div>
+        </div> */} 
       </section>
     </div>
   );
